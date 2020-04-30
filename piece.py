06dd68fb02bd1669afa1bb5f27209ad1622c1182
Avraham Jacobsohn <noreallyimfine@users.noreply.github.com>
2020-04-30 18:05:16 -0400

    set the pieces to initialize with none for the colors

--- piece.py
+++ piece.py
@@ -1,16 +1,15 @@
 class Center(Piece):
-    def __init__(self, side1):
+    def __init__(self, side1=None):
         self.side1 = side1
 
-
 class Edge(Piece):
-    def __init__(self, side1, side2):
+    def __init__(self, side1=None, side2=None):
         self.side1 = side1
         self.side2 = side2
 
 
 class Corner(Piece):
-    def __init__(self, side1, side2, side3):
+    def __init__(self, side1=None, side2=None, side3=None):
         self.side1 = side1
         self.side2 = side2
         self.side3 = side3
