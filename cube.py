1abb8aabb27a01da504f8b7a9141cd7da33cb34c
Avraham Jacobsohn <noreallyimfine@users.noreply.github.com>
2020-04-30 18:42:45 -0400

    piece module and classes are not being used right now. not deleting in case we go back

--- cube.py
+++ cube.py
@@ -1,24 +1,21 @@
 import random
-from piece import Center, Edge, Corner
+from piece import Piece, Center, Edge, Corner
 
 
 class CubeFace:
     def __init__(self):
-        self.center = Center()
-        self.top_left = Corner()
-        self.top_center = Edge()
-        self.top_right = Corner()
-        self.mid_left = Edge()
-        self.mid_right = Edge()
-        self.bot_left = Corner()
-        self.bot_center = Edge()
-        self.bot_right = Corner()
+        self.center = None
+        self.top_left = None
+        self.top_center = None
+        self.top_right = None
+        self.mid_left = None
+        self.mid_right = None
+        self.bot_left = None
+        self.bot_center = None
+        self.bot_right = None
 
 
 class RubiksCube:
-
-    colors = random.shuffle(['w', 'y', 'o', 'r', 'b', 'g'])
-
     def __init__(self):
         self.face1 = CubeFace()
         self.face2 = CubeFace()
@@ -26,12 +23,33 @@ class RubiksCube:
         self.face4 = CubeFace()
         self.face5 = CubeFace()
         self.face6 = CubeFace()
+        self.opposing_faces = [(self.face1, self.face2), (self.face3, self.face4, (self.face5, self.face6))]
 
-
-
-    def randomize_faces():
-        pass
-        # need a list of potential colors
-        # mapping of rules constraining color choices (for after choosing centers)
+    def initialize_centers(self):
+        colors = ['w', 'y', 'o', 'r', 'b', 'g']
 
         # set each faces center to a color, popping off list so to not rechoose
+        # TODO: export to function
+        faces = [self.face1, self.face2, self.face3, self.face4, self.face5, self.face6]
+        for face in faces:
+            face.center = random.choice(colors)
+            colors.remove(face.center)
+
+    def initialize_edges(self):
+        colors = ['w', 'y', 'o', 'r', 'b', 'g']
+        edges = ['top_center', 'mid_left', 'mid_right', 'bot_center']
+        for edge in edges:
+            pass
+
+    def initialize_corners(self):
+        pass
+
+    def initialize_faces(self):
+        # Map a color to each center
+        self.initialize_centers()
+
+        # create edges
+        self.initialize_edges()
+
+        # create corners
+        self.initialize_corners()
--- piece.py
+++ piece.py
@@ -1,16 +1,21 @@
+class Piece:
+    def __init__(self, sides):
+        self.sides = sides
+
+
 class Center(Piece):
-    def __init__(self, side1=None):
+    def __init__(self, side1=None, sides=1):
         self.side1 = side1
 
+
 class Edge(Piece):
-    def __init__(self, side1=None, side2=None):
+    def __init__(self, side1=None, side2=None, sides=1):
         self.side1 = side1
         self.side2 = side2
 
 
 class Corner(Piece):
-    def __init__(self, side1=None, side2=None, side3=None):
+    def __init__(self, side1=None, side2=None, side3=None, sides=1):
         self.side1 = side1
         self.side2 = side2
         self.side3 = side3
-
