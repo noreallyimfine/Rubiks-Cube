f826a3350b395450a0518fc1ae978095e5d62d56
noreallyimfine <avrahamjacobsohn@gmail.com>
2020-05-01 16:35:06 -0400

    add repr and str methods for nicer printing and easier debugging

--- cube.py
+++ cube.py
@@ -12,14 +12,49 @@ class RubiksCube:
                           'left_center': Center(), 'back_center': Center(),
                           'front_left': Edge(), 'front_right': Edge(),
                           'back_left': Edge(), 'back_right': Edge()}
-        self.bot_layer = {'top_center': Center(), 'front_middle': Edge(),
+        self.top_layer = {'top_center': Center(), 'front_middle': Edge(),
                           'front_left': Corner(), 'front_right': Corner(),
                           'back_left': Corner(), 'back_right': Corner(),
                           'back_middle': Edge(), 'left_middle': Edge(),
                           'right_middle': Edge()}
+        
+        self.opposing_colors = []
+    
+    def __repr__(self):
+        return f"""RubiksCube({self.top_layer}, {self.mid_layer}, {self.bot_layer})"""
+
+    def __str__(self):
+        return f"""{self.top_layer}\n\n{self.mid_layer}\n\n{self.bot_layer}"""
 
     def initialize_centers(self):
-        pass
+        # List of colors
+        colors = ['w', 'y', 'o', 'b', 'r', 'g']
+        # list all the center pieces
+        centers = [self.bot_layer['bottom_center'],
+                   self.mid_layer['right_center'],
+                   self.mid_layer['left_center'],
+                   self.mid_layer['front_center'],
+                   self.mid_layer['back_center'],
+                   self.top_layer['top_center']]
+
+        # looping through center pieces
+        for piece in centers:
+            print("Colors:", colors)
+            color = random.choice(colors)
+            # assign it a random choice from the colors
+            piece.side1 = color
+            # remove that color from list
+            colors.remove(color)
+
+        self.opposing_colors.append(
+            (self.bot_layer['bottom_center'].side1, self.top_layer['top_center'].side1)
+            )
+        self.opposing_colors.append(
+            (self.mid_layer['right_center'].side1, self.mid_layer['left_center'].side1)
+            )
+        self.opposing_colors.append(
+            (self.mid_layer['front_center'].side1, self.mid_layer['back_center'].side1)
+            )
 
     def initialize_edges(self):
         pass
--- piece.py
+++ piece.py
@@ -4,18 +4,36 @@ class Piece:
 
 
 class Center(Piece):
-    def __init__(self, side1=None, sides=1):
+    def __init__(self, side1=None):
         self.side1 = side1
+
+    def __repr__(self):
+        return f"Center({self.side1})"
+
+    def __str__(self):
+        return f"Center Piece - ({self.side1})"
 
 
 class Edge(Piece):
-    def __init__(self, side1=None, side2=None, sides=1):
+    def __init__(self, side1=None, side2=None):
         self.side1 = side1
         self.side2 = side2
+
+    def __repr__(self):
+        return f"Edge({self.side1, self.side2})"
+
+    def __str__(self):
+        return f"Edge Piece - ({self.side1}, {self.side2})"
 
 
 class Corner(Piece):
-    def __init__(self, side1=None, side2=None, side3=None, sides=1):
+    def __init__(self, side1=None, side2=None, side3=None):
         self.side1 = side1
         self.side2 = side2
         self.side3 = side3
+
+    def __repr__(self):
+        return f"Corner({self.side1}, {self.side2}, {self.side3})"
+
+    def __str__(self):
+        return f"Corner Piece - ({self.side1}, {self.side2}, {self.side3})"
