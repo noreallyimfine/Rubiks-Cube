23137643ae8038e170d5bfdc49cb20020de03bfe
noreallyimfine <avrahamjacobsohn@gmail.com>
2020-05-01 17:51:38 -0400

    change opposing_colors to sets, now initializing edges without opposing or repeating colors

--- cube.py
+++ cube.py
@@ -47,17 +47,54 @@ class RubiksCube:
             colors.remove(color)
 
         self.opposing_colors.append(
-            (self.bot_layer['bottom_center'].side1, self.top_layer['top_center'].side1)
+            {self.bot_layer['bottom_center'].side1, self.top_layer['top_center'].side1}
             )
         self.opposing_colors.append(
-            (self.mid_layer['right_center'].side1, self.mid_layer['left_center'].side1)
+            {self.mid_layer['right_center'].side1, self.mid_layer['left_center'].side1}
             )
         self.opposing_colors.append(
-            (self.mid_layer['front_center'].side1, self.mid_layer['back_center'].side1)
+            {self.mid_layer['front_center'].side1, self.mid_layer['back_center'].side1}
             )
 
     def initialize_edges(self):
-        pass
+        # list of colors again
+        colors = ['w', 'y', 'o', 'b', 'r', 'g']
+        # dict to keep track of how many times each color has been used
+        colors_count = {color: 0 for color in colors}
+        # list of edges
+        edges = [self.bot_layer['front_middle'],
+                 self.bot_layer['back_middle'],
+                 self.bot_layer['left_middle'],
+                 self.bot_layer['right_middle'],
+                 self.mid_layer['front_left'],
+                 self.mid_layer['front_right'],
+                 self.mid_layer['back_left'],
+                 self.mid_layer['back_right'],
+                 self.top_layer['front_middle'],
+                 self.top_layer['back_middle'],
+                 self.top_layer['left_middle'],
+                 self.top_layer['right_middle']]
+
+        # looping through edges
+        for edge in edges:
+            # random choice from colors list
+            # make sure it's not been picked out already
+            # maybe remove from colors list once picked out
+            first_choice = random.choice(colors) 
+            # assign to side1
+            edge.side1 = first_choice
+
+            # select another choice
+            second_choice = random.choice(colors)
+            # confirm its not the same or the opposing color
+            while second_choice == first_choice or {first_choice, second_choice} in self.opposing_colors:
+                second_choice = random.choice(colors)
+            # assign to second
+            edge.side2 = second_choice
+            # increment both colors dict value
+            colors_count[first_choice] += 1
+            colors_count[second_choice] += 1
+
 
     def initialize_corners(self):
         pass
