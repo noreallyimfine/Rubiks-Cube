47358c36844f6060b99a103a3bfb6e80e02fb5ec
noreallyimfine <avrahamjacobsohn@gmail.com>
2020-05-22 19:50:27 -0400

    begin downward face clockwise turn func

--- cube.py
+++ cube.py
@@ -905,7 +905,9 @@ class RubiksCube:
         '''
         Turn downward face clockwise
         '''
-        pass
+        # CORNERS #
+
+        # EDGES #
     
     def _D_prime(self):
         '''
