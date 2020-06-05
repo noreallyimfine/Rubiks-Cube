aef24f52e7215b6cf74316542a364365d43df132
noreallyimfine <avrahamjacobsohn@gmail.com>
2020-06-05 12:03:13 -0400

    tests werent passing, but detouring to out hanging loop when building out the corners, only happens sometimes

--- test_cube.py
+++ test_cube.py
@@ -8,13 +8,28 @@ class CubeTurnTests(unittest.TestCase):
     def test_L(self):
 
         # Corners
-        top_back_left = self.cube.top_layer['back_left']
-        top_front_left = self.cube.top_layer['front_left']
-        bot_front_left = self.cube.bot_layer['front_left']
-        bot_back_left = self.cube.top_layer['back_left']
+        top_back_left = self.cube.top_layer['back_left'].sides
+        top_front_left = self.cube.top_layer['front_left'].sides
+        bot_front_left = self.cube.bot_layer['front_left'].sides
+        bot_back_left = self.cube.bot_layer['back_left'].sides
 
         # Edges
         mid_back_left = self.cube.mid_layer['back_left']
         top_left_middle = self.cube.top_layer['left_middle']
         mid_front_left = self.cube.mid_layer['front_left']
         bot_left_middle = self.cube.bot_layer['left_middle']
+
+        print("Top front left before turn", top_front_left)
+
+        self.cube._L()
+
+        print("Top front left after turn", top_front_left)
+        self.assertEqual(self.cube.top_layer['back_left'].sides['back'], top_front_left['top'])
+        self.assertEqual(self.cube.top_layer['back_left'].sides['top'], top_front_left['front'])
+        self.assertEqual(self.cube.top_layer['back_left'].sides['left'], top_front_left['left'])
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
