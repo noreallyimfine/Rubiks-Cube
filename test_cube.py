f06af0019f0a77f3fc9742dd09cd1bc68fb3b690
noreallyimfine <avrahamjacobsohn@gmail.com>
2020-06-05 11:27:15 -0400

    store vars of pieces from before turn

--- test_cube.py
+++ test_cube.py
@@ -4,3 +4,13 @@ from cube import RubiksCube
 class CubeTurnTests(unittest.TestCase):
     def setUp(self):
         self.cube = RubiksCube()
+
+    def test_L(self):
+
+        # Corners
+        top_back_left = self.cube.top_layer['back_left']
+        top_front_left = self.cube.top_layer['front_left']
+        bot_front_left = self.cube.bot_layer['front_left']
+        bot_back_left = self.cube.top_layer['back_left']
+
+        # Edges
