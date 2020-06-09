fc3055c9878a1aef40d1d2581dd8dc0b513f839e
noreallyimfine <avrahamjacobsohn@gmail.com>
2020-06-09 12:21:34 -0400

    add second tests

--- test_cube.py
+++ test_cube.py
@@ -14,20 +14,21 @@ class CubeTurnTests(unittest.TestCase):
         bot_back_left = self.cube.bot_layer['back_left'].sides.copy()
 
         # Edges
-        mid_back_left = self.cube.mid_layer['back_left']
-        top_left_middle = self.cube.top_layer['left_middle']
-        mid_front_left = self.cube.mid_layer['front_left']
-        bot_left_middle = self.cube.bot_layer['left_middle']
+        mid_back_left = self.cube.mid_layer['back_left'].copy()
+        top_left_middle = self.cube.top_layer['left_middle'].copy()
+        mid_front_left = self.cube.mid_layer['front_left'].copy()
+        bot_left_middle = self.cube.bot_layer['left_middle'].copy()
 
-        print("Top front left before turn", top_front_left)
 
         self.cube._L()
 
-        print("Top front left after turn", top_front_left)
         self.assertEqual(self.cube.top_layer['back_left'].sides['back'], top_front_left['top'])
         self.assertEqual(self.cube.top_layer['back_left'].sides['top'], top_front_left['front'])
         self.assertEqual(self.cube.top_layer['back_left'].sides['left'], top_front_left['left'])
 
+        self.assertEqual(self.cube.top_layer['front_left'].sides['left'], bot_front_left['left'])
+        self.assertEqual(self.cube.top_layer['front_left'].sides['front'], bot_front_left['bottom'])
+        self.assertEqual(self.cube.top_layer['front_left'].sides['top'], bot_front_left['front'])
 
 
 
