477ff3c9c930c8ace310cce6bebf10cf2653a36f
noreallyimfine <avrahamjacobsohn@gmail.com>
2020-06-10 11:56:52 -0400

    test 4th corner

--- test_cube.py
+++ test_cube.py
@@ -124,5 +124,10 @@ class CubeTurnTests(unittest.TestCase):
         self.assertEqual(self.cube.bot_layer['front_right'].sides['front'], bot_back_right['bottom'])
         self.assertEqual(self.cube.bot_layer['front_right'].sides['right'], bot_back_right['right'])
 
+        self.assertEqual(self.cube.bot_layer['back_right'].sides['back'], top_back_right['top'])
+        self.assertEqual(self.cube.bot_layer['back_right'].sides['bottom'], top_back_right['back'])
+        self.assertEqual(self.cube.bot_layer['back_right'].sides['right'], top_back_right['right'])
+
+
 if __name__ == "__main__":
     unittest.main()
