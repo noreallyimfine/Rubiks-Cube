be1c532811c062afc132900bd7b8cb184fa7fec9
noreallyimfine <avrahamjacobsohn@gmail.com>
2020-06-27 13:06:34 -0400

    move logic tests to new solution tester file

--- test_cube_mechanics.py
+++ test_cube_mechanics.py
@@ -521,39 +521,6 @@ class CubeTurnTests(unittest.TestCase):
         self.assertEqual(self.cube.bot_layer['left_middle'].sides['left'], bot_front_middle['front'])
         self.assertEqual(self.cube.bot_layer['left_middle'].sides['bottom'], bot_front_middle['bottom'])
 
-    def test_make_daisy(self):
-
-        bottom_center = self.cube.bot_layer['bottom_center'].sides['bottom']
-
-        self.cube._make_daisy()
-
-        self.assertEqual(self.cube.top_layer['right_middle'].sides['top'], bottom_center)
-        self.assertEqual(self.cube.top_layer['left_middle'].sides['top'], bottom_center)
-        self.assertEqual(self.cube.top_layer['front_middle'].sides['top'], bottom_center)
-        self.assertEqual(self.cube.top_layer['back_middle'].sides['top'], bottom_center)
-    
-    def test_bottom_cross(self):
-
-        bottom_center = self.cube.bot_layer['bottom_center'].sides['bottom']
-
-        self.cube._bottom_cross()
-
-        self.assertEqual(self.cube.top_layer['right_middle'].sides['top'], bottom_center)
-        self.assertEqual(self.cube.top_layer['left_middle'].sides['top'], bottom_center)
-        self.assertEqual(self.cube.bot_layer['front_middle'].sides['top'], bottom_center)
-        self.assertEqual(self.cube.bot_layer['back_middle'].sides['top'], bottom_center)
-
-    def test_bottom_layer(self):
-
-        # all bottoms match the center
-        bottom_center = self.cube.bot_layer['bottom_center'].sides['bottom']
-        right_center = self.cube.mid_layer['right_center'].sides['right']
-        left_center = self.cube.mid_layer['left_center'].sides['left']
-        front_center = self.cube.mid_layer['front_center'].sides['front']
-        back_center = self.cube.mid_layer['back_center'].sides['back']
-
-        
-        # all 'sides' match their corresponding center
 
 
 
--- test_cube_solution.py
+++ test_cube_solution.py
@@ -5,3 +5,39 @@ from cube import RubiksCube
 class CubeTurnTests(unittest.TestCase):
     def setUp(self):
         self.cube = RubiksCube()
+
+# test solution funcs <- make sure the tests arent interfering with each other
+    
+    def test_make_daisy(self):
+
+        bottom_center = self.cube.bot_layer['bottom_center'].sides['bottom']
+
+        self.cube._make_daisy()
+
+        self.assertEqual(self.cube.top_layer['right_middle'].sides['top'], bottom_center)
+        self.assertEqual(self.cube.top_layer['left_middle'].sides['top'], bottom_center)
+        self.assertEqual(self.cube.top_layer['front_middle'].sides['top'], bottom_center)
+        self.assertEqual(self.cube.top_layer['back_middle'].sides['top'], bottom_center)
+    
+    def test_bottom_cross(self):
+
+        bottom_center = self.cube.bot_layer['bottom_center'].sides['bottom']
+
+        self.cube._bottom_cross()
+
+        self.assertEqual(self.cube.top_layer['right_middle'].sides['top'], bottom_center)
+        self.assertEqual(self.cube.top_layer['left_middle'].sides['top'], bottom_center)
+        self.assertEqual(self.cube.bot_layer['front_middle'].sides['top'], bottom_center)
+        self.assertEqual(self.cube.bot_layer['back_middle'].sides['top'], bottom_center)
+
+    def test_bottom_layer(self):
+
+        # all bottoms match the center
+        bottom_center = self.cube.bot_layer['bottom_center'].sides['bottom']
+        right_center = self.cube.mid_layer['right_center'].sides['right']
+        left_center = self.cube.mid_layer['left_center'].sides['left']
+        front_center = self.cube.mid_layer['front_center'].sides['front']
+        back_center = self.cube.mid_layer['back_center'].sides['back']
+
+        
+        # all 'sides' match their corresponding center
