7e4dcb0db6c114bc086748664127768b4f501621
noreallyimfine <avrahamjacobsohn@gmail.com>
2020-06-27 19:56:31 -0400

    fix class name and add check for bottom face

--- test_cube_solution.py
+++ test_cube_solution.py
@@ -2,7 +2,7 @@ import unittest
 from cube import RubiksCube
 
 # rename that class
-class CubeTurnTests(unittest.TestCase):
+class CubeSolutionTests(unittest.TestCase):
     def setUp(self):
         self.cube = RubiksCube()
 
@@ -25,19 +25,31 @@ class CubeTurnTests(unittest.TestCase):
 
         self.cube._bottom_cross()
 
-        self.assertEqual(self.cube.top_layer['right_middle'].sides['top'], bottom_center)
-        self.assertEqual(self.cube.top_layer['left_middle'].sides['top'], bottom_center)
+        self.assertEqual(self.cube.bot_layer['right_middle'].sides['top'], bottom_center)
+        self.assertEqual(self.cube.bot_layer['left_middle'].sides['top'], bottom_center)
         self.assertEqual(self.cube.bot_layer['front_middle'].sides['top'], bottom_center)
         self.assertEqual(self.cube.bot_layer['back_middle'].sides['top'], bottom_center)
 
     def test_bottom_layer(self):
 
-        # all bottoms match the center
         bottom_center = self.cube.bot_layer['bottom_center'].sides['bottom']
         right_center = self.cube.mid_layer['right_center'].sides['right']
         left_center = self.cube.mid_layer['left_center'].sides['left']
         front_center = self.cube.mid_layer['front_center'].sides['front']
         back_center = self.cube.mid_layer['back_center'].sides['back']
 
+        # all bottoms match the center
+        bottom_face = [
+            self.cube.bot_layer['front_right'].sides['bottom'],
+            self.cube.bot_layer['front_middle'].sides['bottom'],
+            self.cube.bot_layer['front_left'].sides['bottom'],
+            self.cube.bot_layer['back_left'].sides['bottom'],
+            self.cube.bot_layer['back_right'].sides['bottom'],
+            self.cube.bot_layer['back_middle'].sides['bottom'],
+            self.cube.bot_layer['left_middle'].sides['bottom'],
+            self.cube.bot_layer['right_middle'].sides['bottom'],
+        ]
+
+        self.assertTrue(all(side == bottom_center for side in bottom_face))
         
         # all 'sides' match their corresponding center
