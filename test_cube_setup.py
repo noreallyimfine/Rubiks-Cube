608c4f078c4e092312d9b6a5f63de245a402c00e
noreallyimfine <avrahamjacobsohn@gmail.com>
2020-07-07 11:47:34 -0400

    test centers create the opposing colors properlu

--- test_cube_setup.py
+++ test_cube_setup.py
@@ -49,6 +49,44 @@ class CubeSetupTests(unittest.TestCase):
 
         for color in color_dict:
             self.assertEqual(color_dict[color], 9)
+    
+    def test_centers(self):
+
+        top_bottom = (self.cube.top_layer['top_center'].sides['top'], self.cube.bot_layer['bottom_center'].sides['bottom'])
+        right_left = (self.cube.mid_layer['right_center'].sides['right'], self.cube.mid_layer['left_center'].sides['left'])
+        front_back = (self.cube.mid_layer['front_center'].sides['front'], self.cube.mid_layer['back_center'].sides['back'])
+        self.assertIn(top_bottom, self.cube.opposing_colors)
+        self.assertIn(right_left, self.cube.opposing_colors)
+        self.assertIn(front_back, self.cube.opposing_colors)
+        
+    def test_opposing_colors(self):
+        
+        pieces = [
+            self.cube.top_layer['front_right'],
+            self.cube.top_layer['back_right'],
+            self.cube.top_layer['front_left'],
+            self.cube.top_layer['back_left'],
+            self.cube.top_layer['back_middle'],
+            self.cube.top_layer['front_middle'],
+            self.cube.top_layer['right_middle'],
+            self.cube.top_layer['left_middle'],
+            self.cube.mid_layer['front_left'],
+            self.cube.mid_layer['back_left'],
+            self.cube.mid_layer['back_right'],
+            self.cube.mid_layer['front_right'],
+            self.cube.bot_layer['front_right'],
+            self.cube.bot_layer['front_left'],
+            self.cube.bot_layer['back_left'],
+            self.cube.bot_layer['back_right'],
+            self.cube.bot_layer['back_middle'],
+            self.cube.bot_layer['front_middle'],
+            self.cube.bot_layer['right_middle'],
+            self.cube.bot_layer['left_middle'],
+        ]
+
+        # check that no combo of colors on any piece is opposing colors
+        # 
+
 
 if __name__ == "__main__":
     unittest.main()
