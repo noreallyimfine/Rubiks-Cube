5b1742c041ececf7b424e718f4b150ccc1e5695c
noreallyimfine <avrahamjacobsohn@gmail.com>
2020-07-20 10:12:14 -0400

    add initial pseudocode

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 import pygame
+
+# create a screen, light background that doesn't match any of the colors
+
+# create a cube in the screen
+
+# mini cubes in that cube
+
+# ability to rotate the cube
+
+# turn the cube somehow
